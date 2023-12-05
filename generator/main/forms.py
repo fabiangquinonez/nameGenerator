518c3bc8302b8bdb54b45f630ef729067202aa59
Fabian <fabiangquinonez@gmail.com>
2023-12-04 23:41:07 -0800

    committing changes to generator on home page and subsequent post creation

--- generator/main/forms.py
+++ generator/main/forms.py
@@ -13,4 +13,4 @@ class RegisterForm(UserCreationForm):
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ["name", "description"]
+        fields = ['generated_name', "description"]
--- generator/main/models.py
+++ generator/main/models.py
@@ -3,10 +3,10 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE) #if the user referenced here is deleted, the posts they had will be deleted
-    name = models.CharField(max_length=200, default="hello")
+    generated_name = models.CharField(max_length=100)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
-        return self.name + "\n" + self.description
+        return self.name + "\n" + self.description + "\n" + self.generated_name
     
